5fed682eb6c2837a3922ec7a76e938469d2f5887
binbin <tjdqlszld555@naver.com>
2025-01-23 10:50:05 +0900

    친구를 DB에 저장하는 인서트 프렌즈

--- src/main/java/model/FriendDAOimpl.java
+++ src/main/java/model/FriendDAOimpl.java
@@ -1,11 +1,50 @@
 package model;
 
-public class FriendDAOimpl implements FriendDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class FriendDAOImpl implements FriendDAO {
+
+	private static FriendDAOImpl instance =null;
+	
+	private FriendDAOImpl() {}
+	
+	
+	public static FriendDAOImpl getInstance() {
+		if(instance ==null) {
+			instance = new FriendDAOImpl();
+		}
+		return instance;
+		
+	}
+
 
 	@Override
-	public int insertFriend(FriendDTO newFriend) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int insertFriend(FriendDTO newFriend) throws SQLException {
+System.out.println(newFriend.toString() + "을 저장하자!");
+		
+		Connection con = DBConn.getConnection();
+		int result = 0;
+		
+		if (con != null) {
+			String query = "insert into FRIENDS "
+					+ "VALUES(seq_friendNo.nextval, ?, ?, ?)";
+			
+			PreparedStatement pstmt = con.prepareStatement(query);
+			
+			// 매개변수 세팅
+			pstmt.setString(1, newFriend.getFriendName());
+			pstmt.setString(2, newFriend.getMobile());
+			pstmt.setString(3, newFriend.getAddr());
+			
+			// 실행
+			result = pstmt.executeUpdate();
+		
+			DBConn.closeDB(pstmt, con); // DB 닫기
+		}
+		
+		return result;
 	}
 
 }
