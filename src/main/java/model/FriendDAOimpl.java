e29ff713ef0a6938df516f2d345c493d3b2e4f79
binbin <tjdqlszld555@naver.com>
2025-01-23 09:50:27 +0900

    프랜드 DAO d인터페이스 구현체 작성

---
+++ src/main/java/model/FriendDAOimpl.java
@@ -0,0 +1,11 @@
+package model;
+
+public class FriendDAOimpl implements FriendDAO {
+
+	@Override
+	public int insertFriend(FriendDTO newFriend) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
