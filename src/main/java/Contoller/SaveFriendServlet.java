87f913341b0a66736a5b01b025abd4fe98c1284b
binbin <tjdqlszld555@naver.com>
2025-01-24 09:21:18 +0900

    백엔드도 유성이 가능

--- src/main/java/Contoller/SaveFriendServlet.java
+++ src/main/java/Contoller/SaveFriendServlet.java
@@ -14,67 +14,72 @@ import model.FriendDAO;
 import model.FriendDAOImpl;
 import model.FriendDTO;
 
-
-@WebServlet("/save.do") //mapping : /save.do 가 호출되면 현재의 서블릿 객체가 실행됨
+@WebServlet("/save.do") // mapping : /save.do 가 호출되면 현재의 서블릿 객체가 실행됨
 public class SaveFriendServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    
-    public SaveFriendServlet() {
-        super();
 
-    }
+	public SaveFriendServlet() {
+		super();
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		response.sendRedirect("./friend/addFriend.jsp"); // get 방식으로 호출을 할 경우에는 저장할 친구 데이터가 업기 때믄에 친구데이터를 입력 받는 페이지로 강제 이동
-		
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		response.sendRedirect("./friend/addFriend.jsp"); // get 방식으로 호출을 할 경우에는 저장할 친구 데이터가 업기 때믄에 친구데이터를 입력 받는 페이지로 강제
+															// 이동
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// post 방식으로 호출 되었을때
-				System.out.println("친구가 저장됨!!!!!!!!");
-				
-				request.setCharacterEncoding("utf-8");
-				
-	            String name = request.getParameter("friendName");
-	            String mobile = request.getParameter("mobile");
-	            String addr = request.getParameter("addr");
-	            
-	            FriendDTO newFriend = new FriendDTO(name, mobile, addr);
-	            
-	            System.out.println(newFriend.toString());
-	            
-				String redirectPage = null;
-				
-				FriendDAO dao = FriendDAOImpl.getInstance();
-				try {
-					int result = dao.insertFriend(newFriend);
-					
-					if (result == 1) {
-						// 저장 성공
-						redirectPage = request.getContextPath() + "/getFriend.do?isSave=true";
-					} 
-				} catch (SQLException | NamingException e) {
-					// DB 접속에 실패하던, 제약조건위배해서 저장 실패하던지 다 예외로 오게된다. (위 if문에서 else문 필요x)
-					// 저장 실패
-					e.printStackTrace();
-					
-					String msg = e.getMessage();
-			
-					if (e instanceof SQLException && msg.contains("HR.FRIENDS_MOBILE_UQ")) {
-						System.out.println("핸드폰 번호 중복!!!!!!!!!!!!!!!!!!");
-						redirectPage = request.getContextPath() + "/friend/addFriend.jsp?status=mobileUQ";
-					}
-					
+		System.out.println("친구가 저장됨!!!!!!!!");
+
+		request.setCharacterEncoding("utf-8");
+
+		String name = null;
+
+		if (request.getParameter("friendName") != null || !request.getParameter("friendName").equals("")) {
+			name = request.getParameter("friendName");
+		}
+		String mobile = request.getParameter("mobile");
+		String addr = request.getParameter("addr");
+
+		FriendDTO newFriend = new FriendDTO(name, mobile, addr);
+
+		System.out.println(newFriend.toString());
+
+		String redirectPage = null;
+
+		FriendDAO dao = FriendDAOImpl.getInstance();
+		try {
+			int result = dao.insertFriend(newFriend);
+
+			if (result == 1) {
+				// 저장 성공
+				redirectPage = request.getContextPath() + "/getFriend.do?isSave=true";
+			}
+		} catch (SQLException | NamingException e) {
+			// DB 접속에 실패하던, 제약조건위배해서 저장 실패하던지 다 예외로 오게된다. (위 if문에서 else문 필요x)
+			// 저장 실패
+			e.printStackTrace();
+
+			String msg = e.getMessage();
+
+			if (e instanceof SQLException && msg.contains("HR.FRIENDS_MOBILE_UQ")) {
+				System.out.println("핸드폰 번호 중복!!!!!!!!!!!!!!!!!!");
+				redirectPage = request.getContextPath() + "/friend/addFriend.jsp?status=mobileUQ";
+			} else if (e instanceof SQLException && msg.contains("NULL")) {
+				System.out.println("이름이 null!!!!!!!!!!!!!!!!!!");
+				redirectPage = request.getContextPath() + "/friend/addFriend.jsp?status=nameNull";
+			}
 //					redirectPage = request.getContextPath() + "/getFriend.do?isSave=false";
-				}
-				
-				response.sendRedirect(redirectPage);
+		}
+
+		response.sendRedirect(redirectPage);
 	}
 
 }
